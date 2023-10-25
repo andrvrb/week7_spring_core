d6975b3e32f7ce77a79f1f4951a78ce10e36c13e
andrvrb <andr-pnz@mail.ru>
2023-10-25 11:54:47 +0300

    step 18 BeanPostProcessor Before Initialization

--- src/main/java/ru/egartech/database/repository/CompanyRepository.java
+++ src/main/java/ru/egartech/database/repository/CompanyRepository.java
@@ -1,17 +1,11 @@
 package ru.egartech.database.repository;
 
 
+import ru.egartech.bpp.InjectBean;
 import ru.egartech.database.pool.ConnectionPool;
 
 public class CompanyRepository {
 
-    private final ConnectionPool connectionPool;
-
-    public CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
-
-    public static CompanyRepository of(ConnectionPool connectionPool) {
-        return new CompanyRepository(connectionPool);
-    }
+    @InjectBean
+    private ConnectionPool connectionPool;
 }
