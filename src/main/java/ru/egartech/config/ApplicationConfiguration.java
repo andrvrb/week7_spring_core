6a663805c4f266e23759b86d008e4503b8f06008
andrvrb <andr-pnz@mail.ru>
2023-10-26 23:27:32 +0300

    step 31 add WebConfiguration

--- src/main/java/ru/egartech/config/ApplicationConfiguration.java
+++ src/main/java/ru/egartech/config/ApplicationConfiguration.java
@@ -1,13 +1,11 @@
 package ru.egartech.config;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import ru.egartech.database.repository.CrudRepository;
 
+@Import(WebConfiguration.class)
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "ru.egartech",
---
+++ src/main/java/ru/egartech/config/WebConfiguration.java
@@ -0,0 +1,7 @@
+package ru.egartech.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfiguration {
+}
