9d2ae58fb8ff71619d53baf537f0ac73db3f0df1
andrvrb <andr-pnz@mail.ru>
2023-10-27 12:11:16 +0300

    step 36

--- src/main/java/ru/egartech/ApplicationRunner.java
+++ src/main/java/ru/egartech/ApplicationRunner.java
@@ -10,6 +10,12 @@ public class ApplicationRunner {
     public static void main(String[] args) {
 
         try (var context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
+            /*
+            try (var context = new AnnotationConfigApplicationContext()) {
+            context.register(ApplicationConfiguration.class);
+            context.getEnvironment().setActiveProfiles("web", "prod");
+            context.refresh();
+            */
 
             var connectionPool = context.getBean("pool1", ConnectionPool.class);
             System.out.println(connectionPool);
