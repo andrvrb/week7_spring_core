e3e56a7d47548c4c1a309a564580e94a6cd057fd
andrvrb <andr-pnz@mail.ru>
2023-11-05 22:04:48 +0300

    step 41 spring boot

--- src/main/java/ru/egartech/ApplicationRunner.java
+++ src/main/java/ru/egartech/ApplicationRunner.java
@@ -1,28 +1,13 @@
 package ru.egartech;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import ru.egartech.config.ApplicationConfiguration;
-import ru.egartech.database.pool.ConnectionPool;
-import ru.egartech.database.repository.CrudRepository;
-import ru.egartech.database.repository.UserRepository;
-import ru.egartech.service.CompanyService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@SpringBootApplication
 public class ApplicationRunner {
+
     public static void main(String[] args) {
-
-        try (var context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
-            /*
-            try (var context = new AnnotationConfigApplicationContext()) {
-            context.register(ApplicationConfiguration.class);
-            context.getEnvironment().setActiveProfiles("web", "prod");
-            context.refresh();
-            */
-
-            var connectionPool = context.getBean("pool1", ConnectionPool.class);
-            System.out.println(connectionPool);
-
-            var companyService = context.getBean(CompanyService.class);
-            System.out.println(companyService.findById(1));
-        }
+        SpringApplication.run(ApplicationRunner.class, args);
     }
 }
