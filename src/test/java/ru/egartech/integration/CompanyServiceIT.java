8a46cf1a83cc8e1270f915efc714129002c41a46
andrvrb <andr-pnz@mail.ru>
2023-11-18 21:14:44 +0300

    step 53 Integration Testing

--- src/test/java/ru/egartech/integration/CompanyServiceIT.java
+++ src/test/java/ru/egartech/integration/CompanyServiceIT.java
@@ -2,12 +2,13 @@ package ru.egartech.integration;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.egartech.ApplicationRunner;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = ApplicationRunner.class)
+@ContextConfiguration(classes = ApplicationRunner.class, initializers = ConfigDataApplicationContextInitializer.class)
 public class CompanyServiceIT {
     @Test
     void findById() {
