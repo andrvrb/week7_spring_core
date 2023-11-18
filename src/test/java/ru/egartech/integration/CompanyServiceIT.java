bf97baac8411f518a999c33dc146cd29ad3914c4
andrvrb <andr-pnz@mail.ru>
2023-11-18 21:20:22 +0300

    step 54 Integration Testing

--- src/test/java/ru/egartech/integration/CompanyServiceIT.java
+++ src/test/java/ru/egartech/integration/CompanyServiceIT.java
@@ -2,16 +2,34 @@ package ru.egartech.integration;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.egartech.ApplicationRunner;
+import ru.egartech.dto.CompanyReadDto;
+import ru.egartech.service.CompanyService;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = ApplicationRunner.class, initializers = ConfigDataApplicationContextInitializer.class)
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = ApplicationRunner.class, initializers = ConfigDataApplicationContextInitializer.class)
 public class CompanyServiceIT {
+    private static final Integer COMPANY_ID = 1;
+
+    @Autowired
+    private CompanyService companyService;
+
     @Test
     void findById() {
+        var actualResult = companyService.findById(COMPANY_ID);
 
+        assertTrue(actualResult.isPresent());
+
+        var expectedResult = new CompanyReadDto(COMPANY_ID);
+        actualResult.ifPresent(actual -> assertEquals(expectedResult, actual));
     }
 }
