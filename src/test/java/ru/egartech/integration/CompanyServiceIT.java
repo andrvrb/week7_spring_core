1090e16bd2d09a6a41fb23f18fb3ce461a3c50d8
andrvrb <andr-pnz@mail.ru>
2023-11-18 23:50:22 +0300

    step 55 Integration Testing

--- src/test/java/ru/egartech/integration/CompanyServiceIT.java
+++ src/test/java/ru/egartech/integration/CompanyServiceIT.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.egartech.ApplicationRunner;
+import ru.egartech.config.DatabaseProperties;
 import ru.egartech.dto.CompanyReadDto;
 import ru.egartech.service.CompanyService;
 
@@ -15,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@ActiveProfiles("test")
 //@ExtendWith(SpringExtension.class)
 //@ContextConfiguration(classes = ApplicationRunner.class, initializers = ConfigDataApplicationContextInitializer.class)
 public class CompanyServiceIT {
@@ -22,6 +25,8 @@ public class CompanyServiceIT {
 
     @Autowired
     private CompanyService companyService;
+    @Autowired
+    private DatabaseProperties databaseProperties;
 
     @Test
     void findById() {
