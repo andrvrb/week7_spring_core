39f7659a2ca195d9439c517d889a8b6c23bb58dd
andrvrb <andr-pnz@mail.ru>
2023-11-19 14:53:17 +0300

    step 63

--- src/test/java/ru/egartech/integration/service/UserServiceIT.java
+++ src/test/java/ru/egartech/integration/service/UserServiceIT.java
@@ -2,18 +2,18 @@ package ru.egartech.integration.service;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.annotation.DirtiesContext;
 import ru.egartech.database.pool.ConnectionPool;
 import ru.egartech.integration.annotation.IT;
 import ru.egartech.service.UserService;
 
 @IT
 @RequiredArgsConstructor
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class UserServiceIT {
 
     private final UserService userService;
     private final ConnectionPool pool1;
-
 
     @Test
     void test() {
